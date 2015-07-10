736faa6afcfc884e3efecb97d7a273cfa6b74125
Vitaliy Stepanenko <mail@vitaliy.in>
2015-07-10 20:16:30 +0300

    Updated phpDoc

--- src/CollectionReadInterface.php
+++ src/CollectionReadInterface.php
@@ -20,11 +20,47 @@ interface CollectionReadInterface extends IteratorAggregate, Countable
      */
     public function isEmpty();
 
+    /**
+     * Returns first item of the collection or null if collection is empty.
+     *
+     * @return mixed|null
+     */
     public function first();
 
+    /**
+     * Checks that collections contains target item.
+     *
+     * @param $item
+     * @return bool
+     */
     public function contains($item);
 
+    /**
+     *
+     * Iterates over each value in the <b>collection</b>
+     * passing them to the <b>callback</b> function.
+     * If the <b>callback</b> function returns true, the
+     * current value from <b>collection</b> is returned into
+     * the result collection.
+     *
+     * @param callable $callback the callback function to use
+     *
+     * @return static filtered collection
+     */
     public function filter(callable $callback);
 
+
+    /**
+     *
+     * Iterates over each value in the <b>collection</b>
+     * passing them to the <b>callback</b> function.
+     * If the <b>callback</b> function returns true, the
+     * current value from <b>collection</b> is returned.
+     *
+     * @param callable $callback the callback function to use
+     * @param array|null $optionalArguments [optional] additional arguments passed to callback
+     *
+     * @return static filtered collection
+     */
     public function find(callable $callback, array $optionalArguments = null);
 }
