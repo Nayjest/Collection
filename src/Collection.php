737759ff39e0d2890030380977fbbf1d9461fdc0
Vitaliy Stepanenko <mail@vitaliy.in>
2015-07-13 19:28:16 +0300

    Collection::__construct now can accept items array (optional)

--- src/Collection.php
+++ src/Collection.php
@@ -13,4 +13,9 @@ class Collection implements CollectionInterface
     use CollectionDataTrait;
     use CollectionReadTrait;
     use CollectionWriteTrait;
+
+    public function __construct(array $items = null)
+    {
+        $this->setItems($items ?: []);
+    }
 }
