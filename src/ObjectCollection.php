f92c13a3d42a4b7054e626cef54d40df0be98c18
Vitaliy Stepanenko <mail@vitaliy.in>
2015-07-13 17:53:24 +0300

    phpDoc

--- src/ObjectCollection.php
+++ src/ObjectCollection.php
@@ -1,12 +1,21 @@
 <?php
 namespace Nayjest\Collection;
 
+/**
+ * Class ObjectCollection
+ *
+ * Collection of objects.
+ */
 class ObjectCollection implements CollectionInterface
 {
     use CollectionDataTrait;
     use CollectionReadTrait;
     use CollectionWriteTrait;
 
+    /**
+     * @param string $className
+     * @return static
+     */
     public function findByType($className)
     {
         return $this->filter('is_a', [$className]);
