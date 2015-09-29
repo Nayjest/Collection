a65d8e79bb0adc3bfa328f2a7d735c109f9b06fd
Vitaliy Stepanenko <mail@vitaliy.in>
2015-09-29 22:33:29 +0300

    Working on Travis CI

--- update_doc.sh
+++ update_doc.sh
@@ -1,16 +1,17 @@
-if [ "$TRAVIS_PHP_VERSION" == "5.4.37" ] then
-wget http://phpdox.de/releases/phpdox.phar
-chmod +x phpdox.phar
-php phpdox.phar
-git config --global user.email "travis@travis-ci.org"
-git config --global user.name "travis-ci"
-git config --global push.default simple
-git clone --quiet https://${GITHUB_TOKEN}@github.com/Nayjest/Collection.git build/gh-pages
-cd build/gh-pages
-git checkout -b gh-pages origin/gh-pages
-git rm -rf ./
-cp -Rf ../phpdox/html/* ./
-git add -f .
-git commit -m "PHPDocumentor (Travis Build: $TRAVIS_BUILD_NUMBER@$TRAVIS_TAG)"
-git push -fq origin
+if [ "$TRAVIS_PHP_VERSION" == "5.4.37" ]
+    then
+        wget http://phpdox.de/releases/phpdox.phar
+        chmod +x phpdox.phar
+        php phpdox.phar
+        git config --global user.email "travis@travis-ci.org"
+        git config --global user.name "travis-ci"
+        git config --global push.default simple
+        git clone --quiet https://${GITHUB_TOKEN}@github.com/Nayjest/Collection.git build/gh-pages
+        cd build/gh-pages
+        git checkout -b gh-pages origin/gh-pages
+        git rm -rf ./
+        cp -Rf ../phpdox/html/* ./
+        git add -f .
+        git commit -m "PHPDocumentor (Travis Build: $TRAVIS_BUILD_NUMBER@$TRAVIS_TAG)"
+        git push -fq origin
 fi
