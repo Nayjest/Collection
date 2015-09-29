de1c2b5826e8fb7ff6b2c4ae32935dcf206d0db5
Vitaliy Stepanenko <mail@vitaliy.in>
2015-09-29 22:08:54 +0300

    Working on Travis CI, update_doc.sh

--- update_doc.sh
+++ update_doc.sh
@@ -3,6 +3,7 @@ chmod +x phpdox.phar
 php phpdox.phar
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "travis-ci"
+git config --global push.default simple
 git clone --quiet https://${GITHUB_TOKEN}@github.com/Nayjest/Collection.git build/gh-pages
 cd build/gh-pages
 git checkout -b gh-pages origin/gh-pages
