8a251ba02dc84754397cb7c83cbbb6874c4329ac
Vitaliy Stepanenko <mail@vitaliy.in>
2015-09-29 22:00:33 +0300

    Working on Travis CI, update_doc.sh

---
+++ update_doc.sh
@@ -0,0 +1,11 @@
+wget http://phpdox.de/releases/phpdox.phar
+chmod +x phpdox.phar
+php phpdox.phar
+git clone --quiet https://${GITHUB_TOKEN}@github.com/Nayjest/Collection.git build/gh-pages
+cd build/gh-pages
+git checkout -b gh-pages origin/gh-pages
+git rm -rf ./
+cp -Rf ../phpdox/html/* ./
+git add -f .
+git commit -m "PHPDocumentor (Travis Build: $TRAVIS_BUILD_NUMBER@$TRAVIS_TAG)"
+git push -fq origin
