31f12b40056bb8fa8297a5afcd37a4671e94e3a7
Vitaliy Stepanenko <mail@vitaliy.in>
2015-10-23 18:41:38 +0300

    sort() method now performs stable sort

--- src/CollectionReadTrait.php
+++ src/CollectionReadTrait.php
@@ -159,13 +159,35 @@ trait CollectionReadTrait
     }
 
     /**
+     * Sorts collection items.
+     *
+     * This method preserves key order (stable sort) using Schwartzian Transform.
+     * @see http://stackoverflow.com/questions/4353739/preserve-key-order-stable-sort-when-sorting-with-phps-uasort
+     * @see http://en.wikipedia.org/wiki/Schwartzian_transform
+     *
      * @param callable $compareFunction
-     * @return CollectionReadInterface|static
+     * @return CollectionReadInterface|static new collection with sorted items.
      */
     public function sort(callable $compareFunction)
     {
         $items = $this->toArray();
-        usort($items, $compareFunction);
+
+        # Sorting with Schwartzian Transform
+        # If stable sort is not required,
+        # following code can be replaced to usort($items, $compareFunction);
+        $index = 0;
+        foreach ($items as &$item) {
+            $item = [$index++, $item];
+        }
+        usort($items, function($a, $b) use($compareFunction) {
+            $result = call_user_func($compareFunction, $a[1], $b[1]);
+            return $result == 0 ? $a[0] - $b[0] : $result;
+        });
+        foreach ($items as &$item) {
+            $item = $item[1];
+        }
+        # End of sorting with Schwartzian Transform
+
         return $this->createCollection($items);
     }
 
