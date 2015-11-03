095c2ca53cf680110a661cd344bab4368c52ab97
Vitaliy Stepanenko <mail@vitaliy.in>
2015-11-03 14:22:43 +0200

    update_doc.sh fixed: double call of phpdox.phar required to compile doc :(

--- update_doc.sh
+++ update_doc.sh
@@ -3,6 +3,7 @@ if [ "$TRAVIS_PHP_VERSION" == "5.4" ]
         wget http://phpdox.de/releases/phpdox.phar
         chmod +x phpdox.phar
         php phpdox.phar
+        php phpdox.phar
         git config --global user.email "travis@travis-ci.org"
         git config --global user.name "travis-ci"
         git config --global push.default simple
