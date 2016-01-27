1dfa9f799ee62796b5379b29a1c8e95c9d3eeb87
v.stepanenko <mail@vitaliy.in>
2016-01-27 20:10:33 +0200

    phpDoc

--- src/Extended/ObjectCollectionReadInterface.php
+++ src/Extended/ObjectCollectionReadInterface.php
@@ -12,11 +12,27 @@ interface ObjectCollectionReadInterface extends CollectionReadInterface
 
     public function findByType($className);
 
+    /**
+     * Filters collection elements by (parent)class or interface.
+     * @see is_a
+     *
+     * @param string $className
+     * @return self
+     */
     public function filterByType($className);
 
+    /**
+     * @param string $propertyName
+     * @param $value
+     * @return self
+     */
     public function filterByProperty($propertyName, $value);
 
     public function findByProperty($propertyName, $value);
 
+    /**
+     * @param string $propertyName
+     * @return self
+     */
     public function sortByProperty($propertyName);
 }
